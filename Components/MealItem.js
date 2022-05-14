6cf063487e1d0f9d14acdedf4551896070183afc
Schadrack <chadrackngirimana@gmail.com>
2022-05-15 00:20:33 +0200

    display meal title

---
+++ Components/MealItem.js
@@ -0,0 +1,10 @@
+import { View,Text } from "react-native";
+
+
+const MealItem = ({title}) => {
+    return <View>
+        <Text>{ title}</Text>
+  </View>
+}
+
+export default MealItem
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,24 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet,FlatList } from "react-native";
+import MealItem from "../Components/MealItem";
 import { MEALS } from "../data/dummy-data";
 
 const MealsOverviewScreen = ({ route }) => {
-  const categoryId=route.params.categoryId;
+  const categoryId = route.params.categoryId;
+  
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  const renderMealItem = (itemData) => {
+    return <MealItem title={itemData.item.title}/>
+  }
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen- { categoryId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
