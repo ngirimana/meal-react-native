c8a8aae3b853e894dfb5a177168c8b8de255b479
Schadrack <chadrackngirimana@gmail.com>
2022-05-14 23:53:48 +0200

    useNavigation hook

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,16 +1,28 @@
-import { FlatList } from 'react-native';
-import CategoryGridTile from '../Components/CategoryGridTile';
-import { CATEGORIES } from '../data/dummy-data';
+import { FlatList } from "react-native";
+import CategoryGridTile from "../Components/CategoryGridTile";
+import { CATEGORIES } from "../data/dummy-data";
 
-const renderCategoryItem = (itemData) => {
-    return <CategoryGridTile title={itemData.item.title} color={ itemData.item.color} />
-}
-
-const CategoriesScreen = () => {
-    return <FlatList data={CATEGORIES}
-        keyExtractor={(item) => item.id}
-        renderItem={renderCategoryItem}
-        numColumns={2}
+const CategoriesScreen = ({ navigation }) => {
+  const renderCategoryItem = (itemData) => {
+    const pressHandler = () => {
+      navigation.navigate("MealsOverview");
+    };
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+        //navigation={navigation}
+      />
+    );
+  };
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
     />
-}
-export default CategoriesScreen
+  );
+};
+export default CategoriesScreen;
