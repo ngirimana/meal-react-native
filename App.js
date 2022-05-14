14baf3601ab199de31f3c5bf9d631a7c8b4dbc10
Schadrack <chadrackngirimana@gmail.com>
2022-05-14 23:23:25 +0200

    add navigation

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,  } from 'react-native';
-import CategoriesScreen from './screens/CategoriesScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CategoriesScreen from "./screens/CategoriesScreen";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <>
-    <StatusBar style='light'/>
-    <CategoriesScreen />
-  </>
-  
+  return (
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    
-  },
+  container: {},
 });
