79f428bb43873cc6eb7700b72fcd50ce43a917b4
Schadrack <chadrackngirimana@gmail.com>
2022-05-15 19:53:43 +0200

    styling image

--- Components/MealItem.js
+++ Components/MealItem.js
@@ -1,10 +1,68 @@
-import { View,Text } from "react-native";
+import { View, Text, Pressable, Image, StyleSheet } from "react-native";
 
+const MealItem = ({ title, imageUrl, duration, complexity, affordability }) => {
+  return (
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: '#ccc' }}
+        style={({ pressed }) => pressed ? styles.buttonPressed : null}
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image style={styles.image} source={{ uri: imageUrl }} />
+            <Text style={styles.title}>{title}</Text>
+        </View>
+        <View style={styles.details}>
+          <Text style={styles.detailItem}>{duration}m</Text>
+          <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+          <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+          </View>
+          </View>
+      </Pressable>
+    </View>
+  );
+};
 
-const MealItem = ({title}) => {
-    return <View>
-        <Text>{ title}</Text>
-  </View>
-}
+export default MealItem;
 
-export default MealItem
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: "white",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.35,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 16,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow:'hidden'
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 18,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+});
