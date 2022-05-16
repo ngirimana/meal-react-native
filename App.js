a0e7ffb0a7625c758c27971078f6c2dd51ae8cc8
Schadrack <chadrackngirimana@gmail.com>
2022-05-16 11:19:06 +0200

    nested navigation

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet,Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+const DrawerNavigator = () => {
+  return <Drawer.Navigator
+    screenOptions={{
+            headerStyle: { backgroundColor: "#351401" },
+            headerTintColor: "white",
+            sceneContainerStyle: {
+              backgroundColor: "#3f2f25",
+            },
+          }}>
+    <Drawer.Screen name="Categories" component={CategoriesScreen} options={{title:'All Categories'}} />
+    <Drawer.Screen name="Favorites" component={FavoritesScreen}/>
+  </Drawer.Navigator>
+}
 
 export default function App() {
   return (
@@ -23,10 +40,11 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="DrawerScreen"
+            component={DrawerNavigator}
             options={{
-              title: "All Categories",
+             
+              headerShown:false
             }}
           />
           <Stack.Screen
@@ -42,9 +60,10 @@ export default function App() {
           <Stack.Screen
             name="MealDetail"
             component={MealDetailScreen}
-            // options={{
-            //   headerRight: () => { return <Button title="Tap me" onPress={()=>{} } />}
-            // }}
+            options={{
+              title:'About the meal'
+              // headerRight: () => { return <Button title="Tap me" onPress={()=>{} } />}
+            }}
           />
         </Stack.Navigator>
       </NavigationContainer>
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,9 @@
+import { Text } from 'react-native';
+
+const FavoritesScreen = () => {
+  return (
+    <Text>This favorites screen!</Text>
+  )
+}
+
+export default FavoritesScreen
